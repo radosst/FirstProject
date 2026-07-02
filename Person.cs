da19d487b708b4a4801ac0949280a59ff816301b
Radost <joy.ruseva@gmail.com>
2026-07-02 10:51:22 +0300

    Create Person class

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public Person (string name, int age, double salary)
+        {
+            Name = name;
+            Age=age;
+            Salary=salary;
+        }
+        public override string ToString()
+        {
+            return $"Име: {Name}, Възраст: {Age}, Заплата: {Salary:F2}";
+        }
+    }
+}
