0f50a7fe9e71de169b48aaf8f99d6200b5a4eb4d
Radost <joy.ruseva@gmail.com>
2026-07-02 11:55:02 +0300

    Create menu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace FirstProject
+﻿using System.Runtime.CompilerServices;
+
+namespace FirstProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Person> people = LoadPeopleFromFile();
+            bool running = true;
+
+            while( running )
+            {
+                Console.WriteLine("---Избери операция---");
+                Console.WriteLine("1.CREATE (Добавяне на човек)");
+                Console.WriteLine("2.READ (Показване на всички)");
+                Console.WriteLine("3.UPDATE (Промяна на заплатата)");
+                Console.WriteLine("4.DELETE (Изтриване по име) ");
+                Console.WriteLine("5.Изход");
+                Console.WriteLine("Избор: ");
+
+                string choice= Console.ReadLine();
+                Console.WriteLine();
+                
+            }
         }
     }
 }
